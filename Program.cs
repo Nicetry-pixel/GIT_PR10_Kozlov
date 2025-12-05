ce4929496a3db44351c6659c504e920a6096b287
Kozlov Kirill <k7kozlov@gmail.com>
2025-12-05 23:51:17 +0300

    Добавлены две строки кода в файл Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Current date: " + DateTime.Now);
+Console.WriteLine("Group: IS23A");
+Console.WriteLine("Name: Козлов К.Д.");
+
